db3efdeb63bcb00f0762dc975db195384684abf7
tiagotuponi <128715693+tiagotuponi@users.noreply.github.com>
2024-07-31 07:41:51 -0300

    .

--- script.js
+++ script.js
@@ -2,60 +2,110 @@ const caixaPrincipal=document.querySelector(".caixa-principal")
 const caixaPerguntas=document.querySelector(".caixa-perguntas")
 const caixaAlternativas=document.querySelector(".caixa-alternativas")
 const caixaResultado=document.querySelector(".caixa-resultado")
-const caixaResultado=document.querySelector(".caixa-resultado")
+const caixaResultado=document.querySelector(".texto-resultado")
 
 const perguntas = [
     {
-        enunciado:"gab é viado?",
-        alternativas:[
-            "Sim",
-            "Sim, certeza",
-                ]
+        enunciado: "gab e viado",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmação"
+            }
+        ]
     },
     {
-        enunciado:"complete: gabreil  ",
-        alternativas:[
-            "linos",
-            "pauling",
-                ]
+        enunciado: "vocÊ gosta de jogar ",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmação"
+            }
+        ]
     },
     {
-        enunciado:"supra ou skayline?",
-        alternativas:[
-            "supra",
-            "skayline",
-                ]
+        enunciado: "vocÊ gosta de pizza",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmação"
+            }
+        ]
     },
     {
-        enunciado:"turbinado ou aspirado",
-        alternativas:[
-            "tubinado",
-            "aspirado ______",
-                ]
+        enunciado: "vocÊ gosta de sair com os amigos",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmação"
+            }
+        ]
     },
     {
-        enunciado:"o que e um radiador?",
-        alternativas:[
-            "sistema de resfriamento",
-            "caixa de som",
-                ]
-    },  
+        enunciado: "vocÊ gosta de carros",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
 ];
 
-let atual =0;
-let pergunta.Atual;
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = ""; 
 
-function mostraPergunta(){
-    perguntaAtual = perguntas[atual]
-    caixaPerguntas.textContent = perguntaAtual.enunciado
-  
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }  
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = ""; 
+    mostraAlternativas();
 }
-
-function mostraPergunta();
-     for(const alternativaofperguntaAtual.alternativas){
-     const botaoAlternativas = document.createElement("buton");
-     botaoAlternativas.textContent = alternativa;
-     caixaAlternativas.appendChild(botaoAlternativas);
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-]
+
+function respostaSelecionada(opcaoSelecionada){
+            const afirmacoes = opcaoSelecionada.afirmação; /* mudar para opcaoSelecionada.afirmacao*/
+            historiaFinal += afirmacoes + " ";
+            atual++;
+            mostraPergunta();
+      }
+
+function mostraResultado(){
+    caixaPerguntas.textContent = ""
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent ="";
+}
+
+mostraPergunta();
