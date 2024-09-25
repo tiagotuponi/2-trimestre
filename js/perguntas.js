72242ff884f2c3e883243ba3ee047a95c47448d5
tiagotuponi <128715693+tiagotuponi@users.noreply.github.com>
2024-09-25 08:25:34 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+export const perguntas = [
+    {
+        enunciado: "você gosta de fazer amigos ",
+        alternativas: [
+            {
+                texto: "Desigualdades raciais",
+                afirmacao: [
+                    "São perpetuadas por instituições, políticas e normas sociais.",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Discriminação individual",
+                afirmacao: [
+                    "Apenas a discriminações individual e direta afeta a sociedade",
+                    "afirmacao 2"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "principal dano causado pelo racismo",
+        alternativas: [
+            {
+                texto:"fortalece a autoestima das vitimas",
+                afirmacao: [
+                    "racismo enfraquece a autoestima, perpetuando sentimentos de inferioridade",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "contribui para a exclusão social e marginalização de grupos minoritarios ",
+                afirmacao: [
+                    "reforça a desigualdade e estereótipos discriminatórios",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "você apoia racismo?",
+        alternativas: [
+            {
+                texto:"sim, eu apoio o movimento racista",
+                afirmacao: [
+                    "você realmente já ouviu  falar o que é o racismo?  A sua história e quantas milhares de pessoa foram prejudicadas e mortas? ",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Não, sou contra o movimento racista",
+                afirmacao: [
+                    "que legal! concordamos que o racismo não é uma coisa boa ",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
